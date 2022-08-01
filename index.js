809f1b48c6e7ef4a991d3e869ecc78b8ce7151ed
ascife <bizapro123@gmail.com>
2022-08-01 11:17:54 -0500

    v2

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const simpleGit = require("simple-git");
-const git = simpleGit.default();
-
-(async ()=> {
-  await git.init();
-  await git.add("*");
-  await git.commit("v1");
-  await git.push("https://github.com/ccencisoj/ss-example-project.git", "master");
-})();
+console.log("hola mundo");
