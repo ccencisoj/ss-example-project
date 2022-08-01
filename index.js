b1cbea256eb47c4b8698762e9800223aa5cbeb82
ascife <bizapro123@gmail.com>
2022-07-31 22:06:49 -0500

    v1

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const git = simpleGit.default();
   await git.init();
   await git.add("*");
   await git.commit("v1");
-  await git.remote("add origin https://github.com/ccencisoj/ss-example-project.git");
-  await git.push("origin", "master");
+  await git.push("https://github.com/ccencisoj/ss-example-project.git", "master");
 })();
